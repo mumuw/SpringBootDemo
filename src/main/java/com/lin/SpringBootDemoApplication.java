a464607c80ca595a73a5ed47c916cd343a370a1c
mumu <linwl_work@163.com>
2019-01-13 21:01:41 +0800

    19-01-13:Initial springboot demo commit

---
+++ src/main/java/com/lin/SpringBootDemoApplication.java
@@ -0,0 +1,16 @@
+package com.lin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+}
+
